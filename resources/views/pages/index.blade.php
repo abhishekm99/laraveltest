89984d1d42b77599d520d730e8356badd3e2aadb
abhishek malakar <abhishekmalakar4@gmail.com>
2018-08-07 23:38:35 +0530

    Laravel app

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
         <div class="jumbotron text-center">
-                <h1> WELCOME TO TEST OF LARAVEL </h1>
-                <p> LETS SEE HOW THIS WORKS</p>
+                <h1> WELCOME TO TEST OF BLOG FOR LOT </h1>
+                <p> LETS SEE HOW GOOD LARAVEL WORKS</p>
                 <p><a class="btn btn-primary btn-lg" href="/login" role="button">LOGIN<a>  <a class="btn btn-success btn-lg" href="/register" role="button">Register<a></p>
                 </div>
 @endsection
